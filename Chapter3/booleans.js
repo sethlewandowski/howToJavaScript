82b8cd053861149b3476647eb024e380a27c4489
Seth Lewandowski <sethlew@protonmail.com>
2021-02-26 11:46:08 -0700

    adding booleans

---
+++ Chapter3/booleans.js
@@ -0,0 +1,16 @@
+// BOOLEANS //
+
+// Booleans have two possible values - true or false
+
+const TRUE = true; // TRUE is a boolean that equals true
+const FALSE = false; // FALSE is a boolean that equals false
+
+console.log(Boolean(TRUE)) // true
+
+const NUM = 0;
+
+console.log(Boolean(NUM)) // false
+
+const NUM_1 = 1;
+
+console.log(Boolean(NUM_1))
