2645fb93b33a0ab0a42f67255fe1269e16392cce
Seth Lewandowski <sethlew@protonmail.com>
2021-02-26 08:34:24 -0700

    saving work

---
+++ Chapter3/primitiveTypesAndObjects.js
@@ -0,0 +1,28 @@
+// In JS, values are either primitives or objects.
+
+// PRIMITIVES as literals 
+
+let myAge = 28; // 28 is a primitive and literal value
+const MY_NAME = "seth"; // "seth" is a primitive and literal value 
+
+// these primitives (strings and numbers) are IMMUTABLE
+
+// 6 Primitive Types 
+// 1. Number
+// 2. String
+// 3. Boolean
+// 4. Null 
+// 5. Undefined 
+// 6. Symbol
+
+// Immutability doesn't mean that the contents of a variable can't change: 
+let message = "hello!";
+message = "world!";
+
+// message is initalized with an immutable value of "hello!", then assigned a new immutable value of "world!".
+
+// OBJECTS // 
+
+// In addition to these 6 primitives, there are objects.
+// Unlike primitives, objects can take on different forms and values
+
