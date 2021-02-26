27f733cc449cd510ffd8e1e0fcb5fef392db8a7c
Seth Lewandowski <sethlew@protonmail.com>
2021-02-26 12:38:52 -0700

    finish ch3 notes

---
+++ Chapter3/dataTypeConversion.js
@@ -0,0 +1,15 @@
+// convert to numbers
+
+const NUM_STRING = '33.3'; // 33.3 type of string
+const NUM = Number(NUM_STRING); // 33.3 type of number
+
+// convert to string
+
+const NUMBER = 1000;
+const STRING = NUMBER.toString(); // '1000'
+
+// convert to boolean
+
+const N = 0; // falsy value
+const N_1 = !N; // true
+const N_2 = !!N; // false
---
+++ Chapter3/dates.js
@@ -0,0 +1,9 @@
+// DATES //
+
+// js provides us with a built in Date object. Let's start using it to see how it works.
+
+const THIS_MOMENT = new Date();
+console.log(THIS_MOMENT);
+
+const MY_BIRTHDAY = new Date(1992, 4, 28); // months are zero based!
+console.log(MY_BIRTHDAY);
