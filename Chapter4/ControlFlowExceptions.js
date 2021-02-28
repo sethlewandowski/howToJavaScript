4cbb4317ec156063427b05e16173e3721166e355
Seth Lewandowski <sethlew@protonmail.com>
2021-02-28 16:22:06 -0700

    finished chapter 4

---
+++ Chapter4/ControlFlowExceptions.js
@@ -0,0 +1,5 @@
+// There are 4 statements that can alter the normal processing of flow control:
+// 1. Break - breaks out of a loop early
+// 2. continue - skip to the next step in a loop
+// 3. return - Exits the current function
+// 4. throw - indicates an exception that must be caught by an exception handler. 
