9d920a96f12238023502a27b8db3c93b6989f0b9
Seth Lewandowski <sethlew@protonmail.com>
2021-03-04 15:59:27 -0700

    finished concat

---
+++ Arrays/concat.js
@@ -0,0 +1,15 @@
+// array method - concat - adds multiple elements and RETURNS A COPY
+
+let myName = 'seth'
+let myHobby = 'likes to code.'
+
+let phrase = myName + ' ' + myHobby
+console.log(phrase)
+
+let samePhrase = myName.concat(myHobby)
+console.log(samePhrase)
+
+// works the same for arrays
+
+let myArray = ['a', 'b', 'c']
+let secondHalf = []
---
+++ Arrays/methods.js
@@ -0,0 +1,14 @@
+// push, pop, unshift, shift
+// let's manipulate arrays in place
+
+const ARRAY = ['first', 'second', 'third']
+
+ARRAY.push('fourth')
+console.log(ARRAY)
+ARRAY.pop()
+console.log(ARRAY)
+
+ARRAY.unshift('even more first')
+console.log(ARRAY)
+
+console.log(ARRAY.shift())
