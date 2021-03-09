4cd825f406531c63cf9bfe749c6a9ba73b2d071b
Seth Lewandowski <seth@seths-mbp.lan>
2021-03-09 07:28:01 -0700

    copywithin

---
+++ Arrays/copyWithin.js
@@ -0,0 +1,12 @@
+// array.prototype.copyWithin()
+// control backtic launches terminal window in vscode
+// es6 method that takes elements and copies them in place (mods existing  array)
+// not going to change the original size of array
+
+const myArray = [1, 2, 3, 4, 5, 6]
+
+myArray.copyWithin(3, 0)
+console.log(myArray)
+
+myArray.copyWithin(5, 0)
+console.log(myArray)
