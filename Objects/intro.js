405857adaad394f9e5ca7b9f23643994da55e963
Seth Lewandowski <sethlew@protonmail.com>
2021-03-11 10:15:54 -0700

    intro

---
+++ Objects/intro.js
@@ -0,0 +1,15 @@
+// Objects are unordered collections of data
+
+// Arrays are more like...
+
+let array = [a, b, c, d, e]
+
+// whereas objects be more like...
+
+let obj = {
+  a: "a",
+  b: "b",
+  c: "c",
+}
+
+// Remember, with objects, order is not guaranteed.
